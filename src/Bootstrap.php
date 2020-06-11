fabdef229881abeffffc6a4a47815dac950c22c9
Hieu Dong <donghuuhieu1520@gmail.com>
2020-06-11 10:36:03 +0700

    Add dependency injector
    Signed-off-by: Hieu Dong <donghuuhieu1520@gmail.com>

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -21,8 +21,10 @@ if ($environment !== 'production') {
 }
 $whoops->register();
 
-$request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
-$response = new \Http\HttpResponse;
+$injector = include('Dependencies.php');
+
+$request = $injector->make('Http\HttpRequest');
+$response = $injector->make('Http\HttpResponse');
 
 $routeDefinitionCallback = function (\FastRoute\RouteCollector $r) {
   $routes = include('Routes.php');
@@ -34,20 +36,24 @@ $routeDefinitionCallback = function (\FastRoute\RouteCollector $r) {
 $dispatcher = \FastRoute\simpleDispatcher($routeDefinitionCallback);
 
 $routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getPath());
+
 switch ($routeInfo[0]) {
   case \FastRoute\Dispatcher::NOT_FOUND:
-      $response->setContent('404 - Page not found');
-      $response->setStatusCode(404);
-      break;
+    $response->setContent('404 - Page not found');
+    $response->setStatusCode(404);
+    break;
   case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-      $response->setContent('405 - Method not allowed');
-      $response->setStatusCode(405);
-      break;
+    $response->setContent('405 - Method not allowed');
+    $response->setStatusCode(405);
+    break;
   case \FastRoute\Dispatcher::FOUND:
-      $handler = $routeInfo[1];
-      $vars = $routeInfo[2];
-      call_user_func($handler, $vars);
-      break;
+    $className = $routeInfo[1][0];
+    $method = $routeInfo[1][1];
+    $vars = $routeInfo[2];
+    
+    $class = $injector->make($className);
+    $class->$method($vars);
+    break;
 }
 
 foreach ($response->getHeaders() as $header) {
